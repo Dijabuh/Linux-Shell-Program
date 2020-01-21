4f5395222936f98231ce96f4f51a5671f965760b
Daniel Brown <djb16b@my.fsu.edu>
2020-01-20 21:39:01 -0500

    Added in parsing to shell.c file based on parser_help.c

--- shell.c
+++ shell.c
@@ -3,10 +3,116 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+	char** tokens;
+	int numTokens;
+} instruction;
+
+void addToken(instruction* instr_ptr, char* tok);
+void clearInstruction(instruction* instr_ptr);
+void addNull(instruction* instr_ptr);
 
 int main(){
+	char* token = NULL;
+	char* temp = NULL;
 
-	printf("shell \n");
+	instruction instr;
+	instr.tokens = NULL;
+	instr.numTokens = 0;
+
+	while (1) {
+		printf("Temp prompt> ");
+
+		// loop reads character sequences separated by whitespace
+		do {
+			//scans for next token and allocates token var to size of scanned token
+			scanf("%ms", &token);
+			temp = (char*)malloc((strlen(token) + 1) * sizeof(char));
+
+			int i;
+			int start = 0;
+			for (i = 0; i < strlen(token); i++) {
+				//pull out special characters and make them into a separate token in the instruction
+				if (token[i] == '|' || token[i] == '>' || token[i] == '<' || token[i] == '&') {
+					if (i-start > 0) {
+						memcpy(temp, token + start, i - start);
+						temp[i-start] = '\0';
+						addToken(&instr, temp);
+					}
+
+					char specialChar[2];
+					specialChar[0] = token[i];
+					specialChar[1] = '\0';
+
+					addToken(&instr,specialChar);
+
+					start = i + 1;
+				}
+			}
+
+			if (start < strlen(token)) {
+				memcpy(temp, token + start, strlen(token) - start);
+				temp[i-start] = '\0';
+				addToken(&instr, temp);
+			}
+
+			//free and reset variables
+			free(token);
+			free(temp);
+
+			token = NULL;
+			temp = NULL;
+		} while ('\n' != getchar());    //until end of line is reached
+
+		addNull(&instr);
+		//do something based on instruction
+
+		clearInstruction(&instr);
+	}
 
 	return 0;
+}
+
+//reallocates instruction array to hold another token
+//allocates for new token within instruction array
+void addToken(instruction* instr_ptr, char* tok)
+{
+	//extend token array to accomodate an additional token
+	if (instr_ptr->numTokens == 0)
+		instr_ptr->tokens = (char**) malloc(sizeof(char*));
+	else
+		instr_ptr->tokens = (char**) realloc(instr_ptr->tokens, (instr_ptr->numTokens+1) * sizeof(char*));
+
+	//allocate char array for new token in new slot
+	instr_ptr->tokens[instr_ptr->numTokens] = (char *)malloc((strlen(tok)+1) * sizeof(char));
+	strcpy(instr_ptr->tokens[instr_ptr->numTokens], tok);
+
+	instr_ptr->numTokens++;
+}
+
+void addNull(instruction* instr_ptr)
+{
+	//extend token array to accomodate an additional token
+	if (instr_ptr->numTokens == 0)
+		instr_ptr->tokens = (char**)malloc(sizeof(char*));
+	else
+		instr_ptr->tokens = (char**)realloc(instr_ptr->tokens, (instr_ptr->numTokens+1) * sizeof(char*));
+
+	instr_ptr->tokens[instr_ptr->numTokens] = (char*) NULL;
+	instr_ptr->numTokens++;
+}
+
+void clearInstruction(instruction* instr_ptr)
+{
+	int i;
+	for (i = 0; i < instr_ptr->numTokens; i++)
+		free(instr_ptr->tokens[i]);
+
+	free(instr_ptr->tokens);
+
+	instr_ptr->tokens = NULL;
+	instr_ptr->numTokens = 0;
 }
