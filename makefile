bd26bf05cc2d91eada5bb4c298abbbf9898986be
Daniel Brown <djb16b@my.fsu.edu>
2020-01-20 21:55:27 -0500

    Moved parser to its own file

--- makefile
+++ makefile
@@ -1,11 +1,14 @@
 CC = gcc
 TARGET = shell
 
-$(TARGET): $(TARGET).o
-	$(CC) $< -o $@ -std=c99
+$(TARGET): $(TARGET).o parser.o
+	$(CC) $^ -o $@ -std=c99
 
 $(TARGET).o: $(TARGET).c
 	$(CC) -c $< -o $@
+
+parser.o: parser.c
+	$(CC) -c parser.c -o parser.o
 
 clean:
 	rm -rf *.o *~ $(TARGET)
---
+++ parser.h
@@ -0,0 +1,13 @@
+//Header file for parser
+
+
+typedef struct
+{
+	char** tokens;
+	int numTokens;
+} instruction;
+
+void getTokens(instruction* instr);
+void addToken(instruction* instr_ptr, char* tok);
+void clearInstruction(instruction* instr_ptr);
+void addNull(instruction* instr_ptr);
