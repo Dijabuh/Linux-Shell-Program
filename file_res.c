d93da5b61d4d005fd8fa979bfd8819511fc100b4
Daniel Brown <djb16b@my.fsu.edu>
2020-01-21 19:33:52 -0500

    Added in path resolution functions

--- file_res.c
+++ file_res.c
@@ -29,15 +29,14 @@ char* substr(char* str, int start, int end) {
 }
 
 //Splits up a string into an array of strings seperated by a given char
-char** splitString(char* str, char c) {
+void splitString(pathparts* ret, char* str, char c) {
 	int len = strlen(str);
 	int pos = 0;
-	int num = 0;
-	char** ret;
 	char* temp = NULL;
 
 	//initialize the return array
-	ret = (char**) malloc(sizeof(char*));
+	ret->parts = (char**) malloc(sizeof(char*));
+	ret->numParts = 0;
 
 	//loop through str
 	//when we encounter c, put everything from the beggining of str up to c in temp
@@ -47,19 +46,79 @@ char** splitString(char* str, char c) {
 			strcpy(temp, substr(str, pos, i));
 			
 			//now add temp to ret
-			ret = (char**) realloc(ret, (num + 1) * sizeof(char));
+			ret->parts = (char**) realloc(ret, (ret->numParts + 1) * sizeof(char));
 
-			ret[num] = (char*) malloc((strlen(temp) + 1) * sizeof(char));
+			(ret->parts)[ret->numParts] = (char*) malloc((strlen(temp) + 1) * sizeof(char));
 
-			strcpy(ret[num], temp);
+			strcpy((ret->parts)[ret->numParts], temp);
 
-			num++;
+			(ret->numParts)++;
+
 			pos = i+1;
 		}
 		else if (str[i] == c) {
 			pos = i+1;
 		}
 	}
+}
 
-	return ret;
+//Returns the absolute pathname of a given path
+char* getAbsPathname(char* str) {
+	int len = strlen(str);
+	int absLen = 0;
+	char* absPath = NULL;
+	pathparts split;
+	int pos = 0;
+
+	//first check first char of str
+	if (str[0] == '/') {
+		//then we are dealing with an absolute pathname
+		absLen = 1;
+		absPath = (char*) malloc((absLen + 1) * sizeof(char));
+		strcpy(absPath, "/");
+	}
+	else if (str[0] == '~' || (len > 4 && strcmp("$HOME", substr(str, 0, 5)) == 0)) {
+		//then we start at $HOME
+		absLen = strlen(getenv("$HOME"));
+		absPath = (char*) malloc((absLen + 1) * sizeof(char));
+		strcpy(absPath, getenv("$HOME"));
+		pos = 1;
+	}
+	else {
+		//we start at pwd
+		absLen = strlen(getenv("$PWD"));
+		absPath = (char*) malloc((absLen + 1) * sizeof(char));
+		strcpy(absPath, getenv("$PWD"));
+	}
+
+	//get split string
+	splitString(&split, str, '/');
+
+	//check each part of the path and add it on to the abs pathname
+	for(int i = pos; i < split.numParts; i++) {
+		char* curPart;
+		int newlen = strlen(split.parts[i]);
+		curPart = (char*) malloc((newlen + 1) * sizeof(char));
+		strcpy(curPart, split.parts[i]);
+
+		if (strcmp(curPart, ".") == 0) {
+			//in this case, we ignore and do nothing
+		}
+		else if (strcmp(curPart, "..") == 0) {
+			//in this case, we remove one part from our absolute path name
+			//if we are at root, do nothing
+			absLen = strrchr(absPath, '/') - str;
+			absPath = (char*) realloc(absPath, (absLen) * sizeof(char));
+		}
+		else {
+			//otherwise, add the current part to the absolute pathname
+			absLen += newlen;
+			absPath = (char*) realloc(absPath, (absLen + 2) * sizeof(char));
+			strcat(absPath, "/");
+			strcat(absPath, curPart);
+		}
+
+	}
+
+	return absPath;
 }
--- file_res.h
+++ file_res.h
@@ -1,6 +1,14 @@
 //Header for file_res.c
 #include <stdbool.h>
 
+typedef struct
+{
+	char** parts;
+	int numParts;
+} pathparts;
+
 bool isPath(char* str);
 char* substr(char* str, int start, int end);
-char** splitString(char* str, char c);
+void splitString(pathparts* ret, char* str, char c);
+char* getAbsPathname(char* str);
+char* getAbsPathname(char* str);
