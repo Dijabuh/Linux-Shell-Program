f0b15d4714ba0d189afb94fc79ad17e87d8e7b08
Garett Anderson <ga17@my.fsu.edu>
2020-01-24 19:39:14 -0500

    Added function to setup and call redirection

--- redirection.c
+++ redirection.c
@@ -1,33 +1,39 @@
+#include <fcntl.h>
 #include <unistd.h>
 
+#include "redirection.h"
 #include "execution.h"
 
 void redirection(char** cmd){
+	/*
+	bool input_redir = (redirection_symbol == '<');
+	int file_desc;
 
-	if(/*redirection symbol*/){
-		redirect_input(cmd);
-	} else{
-		redirect_output(cmd);
-	}
+	if(input_redir)
+		file_desc = open(f_input, O_RDONLY);
+	else
+		file_desc = open(f_input, O_WRONLY);
+
+	link_cmd_path(cmd, file_desc, input_redir);
+	*/
 }
 
-void redirect_input(char* f_input, char* cmd){
-	
-	// Opens file to take input from
-	int file_desc = open(f_input);
+void link_cmd_path(char** cmd, int file_desc, bool io_dir){
 
 	if(fork() == 0){
-		close(STDIN_FILENO);
+		
+		if(io_dir)
+			close(STDIN_FILENO);
+		else
+			close(STDOUT_FILENO);
+
 		dup(file_desc);
 		close(file_desc);
+		
 		// Execute process here
+		execute(cmd);
 	} else{
 		// Parent?
 		close(file_desc);
 	}
-}
-
-void redirect_output(char** cmd){
-
-
 }
--- redirection.h
+++ redirection.h
@@ -1,1 +1,5 @@
-int redirection
+#include <stdbool.h>
+
+void redirection(char** cmd);
+
+void link_cmd_path(char** cmd, int file_desc, bool io_dir);
