f8d3b5d368db37a6e6a7f3415cdb03f2e58ce198
Garett Anderson <ga17@my.fsu.edu>
2020-01-26 23:56:04 -0500

    Added fn to check parsing rules, returns 1 of 4 cases

--- redirection.c
+++ redirection.c
@@ -5,7 +5,10 @@
 #include "redirection.h"
 #include "execution.h"
 
-void redirection(char** cmd){
+void redirection(char** cmd, int tokens){
+
+	if(!parsing_rules(cmd, tokens)){ return; } // Failed parsing rules
+
 	/*
 	bool input_redir = (redirection_symbol == '<');
 	int file_desc;
@@ -40,4 +43,71 @@ void link_cmd_path(char** cmd, int file_desc, bool io_dir){
 		// Parent?
 		close(file_desc);
 	}
+}
+
+int parsing_rules(char** cmd, int tokens){
+	
+	/* Rules
+	 1. 1 less <>'s than cmds
+	 2. No more than 2 <>'s
+	 3. If 2 <>, must alternate
+	 4. Can't start or end with <>'s
+	 5. Must alternate <>'s and cmds
+	*/
+
+	int cmd_counter = redir_counter = 0;
+	
+	// Tracks if on cmd/<> for rule 5
+	bool alt = true;	// true = don't accept <>
+	
+	// Keeps track of 2 <>'s for rule 3
+	int multi_redir = -1; // -1=none, 0=<, 1=>
+
+	// Checks for rule 4
+	if(cmd[0] == '<' || cmd[0] == '>')
+		return -1;
+
+	for(int i = 0; i < tokens; ++i){
+		
+		if(cmd[i] == '<' || cmd[i] == '>'){
+			// Checks for rule 2
+			if(redir_counter > 1){ return -1; }
+
+			if(!alt){
+				// Checks for rule 3
+				if(multi_redir == 0)
+					if(cmd[i] == '<'){ return -1; }
+				else if(mutli_redir == 1)
+					if(cmd[i] == '>'){ return -1; }
+				else{
+					// Sets which <> called
+					if(cmd[i] == '<'){ multi_redir = 0; }
+					else{ multi_redir = 1; }
+				}
+
+				alt = true; // tracks for 5
+				++redir_counter;
+				// Here bc cmd may have flags/args after
+				++cmd_counter;
+			} else{
+				return -1; // violates 5
+			}
+		} else{
+			alt = false; // command currently
+		}
+	}
+
+	// Checking if ended with command string
+	if(!alt){ ++cmd_counter; }
+	else{ return -1; } // violates 4
+
+	if(redir_counter + 1 != cmd_counter){ return -1; } // violates 1
+
+	if(redir_counter == 1){ return multi_redir; } // returns < or >
+	else if(redir_counter == 2){
+		// Returns either for < > or > <
+		if(multi_redir == 0){ return 2; }
+		else if(multi_redir == 1){ return 3; }
+		else{ return -1; }
+	} else{ return -1; }
 }
